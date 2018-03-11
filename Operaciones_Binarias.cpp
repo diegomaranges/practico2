53e18a3d715f9551d6c7c3d692bfdc65890d1705
Diego <32415456+diegomaranges@users.noreply.github.com>
2018-03-11 15:22:43 -0300

    define operations binary

--- Operaciones_Binarias.cpp
+++ Operaciones_Binarias.cpp
@@ -1,2 +1,24 @@
 #include "Operaciones_Binarias.h"
 
+lint and_bin(lint one, lint two)
+{
+    return one & two;
+}
+
+
+lint or_bin(lint one, lint two)
+{
+    return one | two;
+}
+
+
+lint sleft(lint one)
+{
+    return one<<2;
+}
+
+
+lint sright(lint one)
+{
+    return one>>2;
+}
--- Operaciones_Binarias.h
+++ Operaciones_Binarias.h
@@ -1,7 +1,18 @@
 #ifndef OPERACIONES_BINARIAS_H
 #define OPERACIONES_BINARIAS_H
+//Operaciones binarias
+typedef long int lint;
 
+lint and_bin(lint one, lint two);
+//AND binario
 
-//cout << "* Operaciones binarias                            *" << endl;
+lint or_bin(lint one, lint two);
+//OR binario
+
+lint sleft(lint one);
+//Desplazamiento de 2 bits a la izquierda
+
+lint sright(lint one);
+// Desplazamiento de 2 bits a la derecha
 
 #endif // OPERACIONES_BINARIAS_H
