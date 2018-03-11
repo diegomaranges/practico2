c6d015ed11ccb5ff2d45e22b09da851ae0b83e85
Diego <32415456+diegomaranges@users.noreply.github.com>
2018-03-11 15:23:15 -0300

    define operations mathematics

--- Operaciones_Matenaticas.cpp
+++ Operaciones_Matenaticas.cpp
@@ -1,3 +1,29 @@
 #include "Operaciones_Matenaticas.h"
 
+#define PI 3.14159
 
+float cal_square(float num1)
+{
+    return num1 * num1;
+}
+//El cuadrado de un numer
+
+float cal_addition(float num1, float num2)
+{
+    return num1 + num2;
+}
+//La suma de dos numeros
+
+float circle_area(float radius)
+{
+    return PI * radius * radius;
+}
+//El area de un circulo
+
+float maximal(float num1, float num2)
+{
+    if(num1 > num2)
+        return num1;
+    return num2;
+}
+//El maximo de dos numeros
--- Operaciones_Matenaticas.h
+++ Operaciones_Matenaticas.h
@@ -1,10 +1,16 @@
 #ifndef OPERACIONES_MATENATICAS_H
 #define OPERACIONES_MATENATICAS_H
 
+float cal_square(float num1);
+//El cuadrado de un numer
 
-//cout << "* El cuadrado de un numer                         *" << endl;
-//cout << "* La suma de dos numeros                          *" << endl;
-//cout << "* El area de un circulo                           *" << endl;
-//cout << "* El maximo de dos numeros                        *" << endl;
+float cal_addition(float num1, float num2);
+//La suma de dos numeros
+
+float circle_area(float radius);
+//El area de un circulo
+
+float maximal(float num1, float num2);
+//El maximo de dos numeros
 
 #endif // OPERACIONES_MATENATICAS_H
