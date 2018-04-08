741a432dae480e7a8eff08a14339621b23331cb1
Diego <32415456+diegomaranges@users.noreply.github.com>
2018-04-08 18:01:14 -0300

    remove qt proyect
    I save in c\user\diego\sourse just in case

--- practico2 y 3/Operaciones_String.cpp
+++
@@ -1,8 +0,0 @@
-#include "Operaciones_String.h"
-
-bool compare_string(string string1, string string2)
-{
-    if (string1.compare(string2) == 0)
-        return true;
-    return false;
-}
--- practico2 y 3/Operaciones_logicas.cpp
+++
@@ -1,9 +0,0 @@
-#include "Operaciones_logicas.h"
-
-template <typename T>
-void my_swap(T &n1, T &n2)
-{
-    swap(n1, n2);
-}
-
-template void my_swap<string>(string &n1 , string &n2);
